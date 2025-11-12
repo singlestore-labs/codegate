19ccca969deac617b802066a1542669a72e9d3d1
Siqing <szheng@singlestore.com>
2025-11-12 14:04:48 +0000

    first-version

---
+++ codegate.go
@@ -0,0 +1,118 @@
+package codegate
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+	"sync"
+)
+
+// Gate is a code gate, allowing code to be selectively enabled or disabled.
+// Gate currently contains only a Name field, but may grow in the future to
+// include other metadata about the gate.
+type Gate struct {
+	name    string
+	enabled bool
+}
+
+const (
+	gateNameMaxLength = 100
+	gateEnvVarPrefix  = "DISABLE_"
+)
+
+var (
+	validName     = regexp.MustCompile("^[A-Za-z][A-Za-z0-9_]*$")
+	usedNames     = map[string]struct{}{}
+	nameLock      = sync.Mutex{}
+	disabledGates []string
+)
+
+func init() {
+	// Declare a gate for use in test cases. This gate is disabled in the code-gate-toggles configmap.
+	_ = New("DisabledTestCodeGate")
+}
+
+// New creates a code gate. Code gate names must be globally unique and should
+// be defined in static initializers. For example,
+//
+//	var gateRBACDeleteOrphanedGrants = codegate.New("RBACDeleteOrphanedGrants")
+//
+// The gate name must start with an alpha character and contain only alphanumeric
+// or underbar characters. Title casing (i.e., Go style naming) and standard prefix
+// for each code domain (e.g., "RBAC" for RBAC related behaviors) is recommended.
+// New panics if the name is missing, invalid, or is a duplicate.
+func New(name string) Gate {
+	if !validName.MatchString(name) || len(name) > gateNameMaxLength {
+		panic(fmt.Errorf(`code gate name (%s) is invalid. Code gate names must begin with an alpha, contain only alphanumerics or underbars, and be no more than %d characters in length`,
+			name, gateNameMaxLength))
+	}
+	nameLock.Lock()
+	defer nameLock.Unlock()
+	if _, ok := usedNames[name]; ok {
+		panic(fmt.Errorf(`code gate name (%s) has been used twice. Code gate names must be unique`, name))
+	}
+	usedNames[name] = struct{}{}
+	_, ok := os.LookupEnv(gateEnvVarPrefix + name)
+	return Gate{
+		name:    name,
+		enabled: !ok,
+	}
+}
+
+// Enabled returns true unless the code gate been disabled. Code gates
+// control system-wide features, bug fixes, or other behavioral changes
+// that may need to be disabled due to unforeseen failures or side effects.
+//
+// Example usage:
+//
+//	if gateRBACDeleteOrphanedGrants.Enabled() {
+//		deleteOrphanedGrants(ctx)
+//	} else {
+//		// execute old code replaced by new code above, if any
+//	}
+//
+// NOTE: Code gates are currently disabled by defining the DISABLE_<gate-name>
+// environment variable for the process(es) implementing the code gate. Any Go
+// process may implement code gates. This implementation may change in the
+// future. Runtime code outside this package should not have any dependencies on
+// the environment variable implementation.
+func (gate Gate) Enabled() bool {
+	return gate.enabled
+}
+
+// Name returns the code gate name.
+func (gate Gate) Name() string {
+	return gate.name
+}
+
+// String returns a string representation of the code gate and its state.
+func (gate Gate) String() string {
+	label := fmt.Sprintf("code gate %s", gate.name)
+	if gate.enabled {
+		return label + " (enabled)"
+	}
+	return label + " (disabled)"
+}
+
+func DisabledGates() []string {
+	if disabledGates == nil {
+		refreshDisabledGates()
+	}
+
+	return disabledGates
+}
+
+// refreshDisabledGates refreshes the list of disabled gates by going through
+// the environment variables.
+func refreshDisabledGates() {
+	disabledGates = []string{}
+
+	// Get all disabled code gates from the environment variables.
+	for _, env := range os.Environ() {
+		envName, _, _ := strings.Cut(env, "=")
+		if strings.HasPrefix(envName, gateEnvVarPrefix) {
+			disabledGates = append(disabledGates, strings.TrimPrefix(envName, gateEnvVarPrefix))
+		}
+	}
+}
---
+++ codegate_test.go
@@ -0,0 +1,89 @@
+package codegate
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Code gates are somewhat troublesome to test due to the reliance on a static environment variable
+// at the time of the gate creation. All tests run in the same environment, so setting an environment
+// variable in one test may affect other tests (depending on order.) DisableGates() testing only
+// works because it is implemented to dynamically inspect the environment at call time and does not
+// cache results.
+
+func TestNoDisabledGates(t *testing.T) {
+	gateTestFoo := New("FOO")
+	require.True(t, gateTestFoo.Enabled(), "arbitrary code behavior should be enabled by default")
+	require.NotContains(t, DisabledGates(), "FOO")
+}
+
+func TestGateNames(t *testing.T) {
+	// valid names
+	_ = New("A")
+	_ = New("Z123")
+	_ = New("A_B______C_D____")
+	_ = New("ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789")
+	_ = New("RBACDeleteOrphanedGrants")
+	_ = New("RBAC_DELETE_ORPHANED_GRANTS")
+	_ = New(strings.Repeat("Z", 100))
+
+	// invalid names
+	require.Panics(t, func() { New("") })
+	require.Panics(t, func() { New("1") })
+	require.Panics(t, func() { New("%") })
+	require.Panics(t, func() { New("A$") })
+	require.Panics(t, func() { New("A-A") })
+	require.Panics(t, func() {
+		New(strings.Repeat("A", 101))
+	})
+
+	// duplicate name
+	require.Panics(t, func() { New("A") })
+}
+
+func TestDisableOneGate(t *testing.T) {
+	_ = os.Setenv("DISABLE_S2CODE_Bar", "disabled")
+
+	// refresh disabled gates to pick up the changes to the environment
+	// variables
+	refreshDisabledGates()
+
+	gateTestBar := New("Bar")
+	require.False(t, gateTestBar.Enabled(), "Bar should be disabled")
+	require.True(t, New("Bar2").Enabled(), "Other gates should be enabled")
+	require.Contains(t, DisabledGates(), "Bar")
+}
+
+func TestDisableMultipleGates(t *testing.T) {
+	// create some random environment variables
+	_ = os.Setenv("NOISE", "LOUD")
+	_ = os.Setenv("MORE_NOISE", "LOUDER")
+	_ = os.Setenv("DISABLE_CODE", "")
+
+	// disable two gates
+	_ = os.Setenv("DISABLE_S2CODE_Baz1", "disabled")
+	_ = os.Setenv("DISABLE_S2CODE_Baz3", "disabled")
+
+	// refresh disabled gates to pick up the changes to the environment
+	// variables
+	refreshDisabledGates()
+
+	// define four gates
+	gateTestBaz1 := New("Baz1")
+	gateTestBaz2 := New("Baz2")
+	gateTestBaz3 := New("Baz3")
+	gateTestBaz4 := New("Baz4")
+
+	require.False(t, gateTestBaz1.Enabled(), "Baz1 should be disabled")
+	require.True(t, gateTestBaz2.Enabled(), "Baz2 should be enabled")
+	require.False(t, gateTestBaz3.Enabled(), "Baz3 should be disabled")
+	require.True(t, gateTestBaz4.Enabled(), "Baz4 should be enabled")
+
+	require.Contains(t, DisabledGates(), "Baz1")
+	require.NotContains(t, DisabledGates(), "Baz2")
+	require.Contains(t, DisabledGates(), "Baz3")
+	require.NotContains(t, DisabledGates(), "Baz4")
+}
